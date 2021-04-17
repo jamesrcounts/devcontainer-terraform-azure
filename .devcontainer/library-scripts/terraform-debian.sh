49b3d2ebd9c5b7a247fdb332a9676bc27841ae5a
Jim Counts <jamesrcounts@outlook.com>
2021-04-17 07:34:39 -0700

    Update devcontainer from upstream

--- .devcontainer/library-scripts/terraform-debian.sh
+++ .devcontainer/library-scripts/terraform-debian.sh
@@ -5,13 +5,15 @@
 #-------------------------------------------------------------------------------------------------------------
 #
 # Docs: https://github.com/microsoft/vscode-dev-containers/blob/master/script-library/docs/terraform.md
+# Maintainer: The VS Code and Codespaces Teams
 #
-# Syntax: ./terraform-debian.sh [terraform version] [tflint version]
-set -euo pipefail
+# Syntax: ./terraform-debian.sh [terraform version] [tflint version] [terragrunt version]
 
 TERRAFORM_VERSION=${1:-"latest"}
-TERRAFORM_VERSIONS=${2:-"latest"}
-TFLINT_VERSION=${3:-"latest"}
+TFLINT_VERSION=${2:-"latest"}
+TERRAGRUNT_VERSION=${3:-"latest"}
+
+set -e
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
@@ -22,13 +24,14 @@ if [ "${TERRAFORM_VERSION}" = "latest" ] || [ "${TERRAFORM_VERSION}" = "lts" ] |
     TERRAFORM_VERSION=$(curl -sSL https://releases.hashicorp.com/terraform/ | grep -m1 -oE '>terraform_[0-9]+\.[0-9]+\.[0-9]+<' | sed 's/^>terraform_\(.*\)<$/\1/')
 fi
 
-if [ "${TERRAFORM_VERSIONS}" = "latest" ] || [ "${TERRAFORM_VERSIONS}" = "lts" ] || [ "${TERRAFORM_VERSIONS}" = "current" ]; then
-    TERRAFORM_VERSIONS="${TERRAFORM_VERSION}"
-fi
-
 if [ "${TFLINT_VERSION}" = "latest" ] || [ "${TFLINT_VERSION}" = "lts" ] || [ "${TFLINT_VERSION}" = "current" ]; then
     LATEST_RELEASE=$(curl -sSL -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/terraform-linters/tflint/releases?per_page=1&page=1")
     TFLINT_VERSION=$(echo ${LATEST_RELEASE} | grep -oE 'tag_name":\s*"v[^"]+' | sed -n '/tag_name":\s*"v/s///p')
+fi
+
+if [ "${TERRAGRUNT_VERSION}" = "latest" ] || [ "${TERRAGRUNT_VERSION}" = "lts" ] || [ "${TERRAGRUNT_VERSION}" = "current" ]; then
+    LATEST_RELEASE=$(curl -sSL -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/gruntwork-io/terragrunt/releases?per_page=1&page=1")
+    TERRAGRUNT_VERSION=$(echo ${LATEST_RELEASE} | grep -oE 'tag_name":\s*"v[^"]+' | sed -n '/tag_name":\s*"v/s///p')
 fi
 
 # Install curl, unzip if missing
@@ -40,27 +43,25 @@ if ! dpkg -s curl ca-certificates unzip > /dev/null 2>&1; then
     apt-get -y install --no-install-recommends curl ca-certificates unzip
 fi
 
-# Install Terraform, tflint
+# Install Terraform, tflint, Terragrunt
 echo "Downloading terraform..."
 mkdir -p /tmp/tf-downloads
-for v in $TERRAFORM_VERSIONS
-do
-    dl="/tmp/tf-downloads/${v}"
-    mkdir -p "${dl}"
-    curl -sSL -o ${dl}/terraform.zip https://releases.hashicorp.com/terraform/${v}/terraform_${v}_linux_amd64.zip
-    unzip ${dl}/terraform.zip
-    mv -f terraform /usr/local/bin/terraform-${v}
-done
-
-# set default terraform version
-ln -s /usr/local/bin/terraform-${TERRAFORM_VERSION} /usr/local/bin/terraform
-
+curl -sSL -o /tmp/tf-downloads/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip /tmp/tf-downloads/terraform.zip
+mv -f terraform /usr/local/bin/
 
 if [ "${TFLINT_VERSION}" != "none" ]; then
     echo "Downloading tflint..."
     curl -sSL -o /tmp/tf-downloads/tflint.zip https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip
     unzip /tmp/tf-downloads/tflint.zip
     mv -f tflint /usr/local/bin/
+fi
+
+if [ "${TERRAGRUNT_VERSION}" != "none" ]; then
+    echo "Downloading Terragrunt..."
+    curl -sSL -o /tmp/tf-downloads/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/v${TERRAGRUNT_VERSION}/terragrunt_linux_amd64
+    chmod a+x /tmp/tf-downloads/terragrunt
+    mv -f /tmp/tf-downloads/terragrunt /usr/local/bin/
 fi
 
 rm -rf /tmp/tf-downloads
