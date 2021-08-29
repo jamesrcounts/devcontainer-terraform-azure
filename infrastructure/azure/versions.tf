3a5aa97c6f0f279235dc5c1cc8a0d3e5fb81e6a3
Jim Counts <jamesrcounts@outlook.com>
2021-08-29 06:47:44 -0700

    Refresh

--- infrastructure/azure/versions.tf
+++ infrastructure/azure/versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 0.15"
+  required_version = ">= 1"
 
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 1.0.0"
+      version = "~> 1"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.32"
+      version = "~> 2"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0.0"
+      version = "~> 3"
     }
   }
 }
